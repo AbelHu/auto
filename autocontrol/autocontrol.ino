af70ffe7e42d2f7197413f6d80d092e619c484ef
Abel Hu <abelch@microsoft.com>
2017-08-08 19:46:54 +0800

    Add Gree controller

---
+++ autocontrol/autocontrol.ino
@@ -0,0 +1,110 @@
+// Compare PM 2.5 detectors: A4 vs. G3
+// Compare temperature sensors: DHT-22 vs. SHT-31
+// Runs on the Arduino part of LinkIt Smart 7688 Duo
+
+// A4 pin 1   -> +5V
+// A4 pin 2   -> GND
+// A4 pin 5   -> 7688 Duo pin D11
+// G3 pin 5   -> 7688 Duo pin D9
+// SHT-31 SDA -> 7688 Duo pin D2
+// SHT-31 SCL -> 7688 Duo pin D3
+// DHT-22 DAT -> 7688 Duo pin D4
+
+#include <Wire.h>
+#include <Adafruit_SHT31.h>
+#include <IRremote.h>
+
+# define MODE_ZERO 0
+# define MODE_UP   1
+# define MODE_DOWN 2
+
+int mode = MODE_ZERO;
+
+# define HUM_MODE_ZERO 0
+# define HUM_MODE_ON   1
+# define HUM_MODE_OFF  2
+
+int hum_mode = HUM_MODE_ZERO;
+
+IRsend irsend;
+
+// #define SERIAL_DEBUG  true  // Comment to disable it
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+# define MYPRINT(...)    ({ \
+  Serial.print(__VA_ARGS__);  \
+  Serial1.print(__VA_ARGS__); \
+})
+# define MYPRINTLN(...)  ({ \
+  Serial.println(__VA_ARGS__);  \
+  Serial1.println(__VA_ARGS__); \
+})
+
+const unsigned long GREE_OPEN[2] = {0x9050040A, 0x4000F};
+const unsigned long GREE_CLOSE[2] = {0x8050040A, 0x4000E};
+const unsigned long GREE_25[2] = {0x9090040A, 0x40007};
+const unsigned long GREE_26[2] = {0x9050040A, 0x4000F};
+const unsigned long GREE_27[2] = {0x90D0040A, 0x40000};
+
+void setup() {
+  Serial1.begin(57600);
+  Serial.begin(9600);
+
+  delay(60000);
+  if (!sht31.begin(0x44)) {
+    MYPRINTLN("LOG: Couldn't find SHT31");
+    while(1) {
+      MYPRINTLN("LOG: Fail forever");
+      delay(1000);
+    }
+  }
+  MYPRINTLN("LOG: Found SHT31");
+}
+
+void loop() {
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+  if(!isnan(t) && !isnan(h)) {
+    MYPRINT("REC: ");
+    MYPRINT(t);
+    MYPRINT(",");
+    MYPRINTLN(h);
+    if (t < 26.0) {
+      if (mode != MODE_UP) {
+        irsend.sendGREE(GREE_27[0], GREE_27[1]);
+        MYPRINTLN("LOG: Temperature < 26.00, turn up to 27");
+        mode = MODE_UP;
+      }
+    } else if (t > 26.5) {
+      if (mode != MODE_DOWN) {
+        irsend.sendGREE(GREE_25[0], GREE_25[1]);
+        MYPRINTLN("LOG: Temperature > 26.50, turn down to 25");
+        mode = MODE_DOWN;
+      }
+    } else {
+      if (mode != MODE_ZERO) {
+        mode = MODE_ZERO;
+        irsend.sendGREE(GREE_26[0], GREE_26[1]);
+        MYPRINTLN("LOG: Temperature is good, change to 26");
+      }
+    }
+
+    if (h < 60.0) {
+      if (hum_mode != HUM_MODE_ON) {
+        MYPRINTLN("CMD: TURN_ON_SWITCH");
+        hum_mode = HUM_MODE_ON;
+      }
+    } else if (h > 65.0) {
+      if (hum_mode != HUM_MODE_OFF) {
+        MYPRINTLN("CMD: TURN_OFF_SWITCH");
+        hum_mode = HUM_MODE_OFF;
+      }
+    } else {
+      MYPRINTLN("LOG: Do not operate switch");
+    }
+  } else {
+    MYPRINTLN("LOG: SHT31 - Cannot get temperature...");
+  }
+  delay(60000);
+}
+
---
+++ autocontrol/autocontrol.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import sys
+import time
+import socket
+import datetime
+import serial
+import re
+
+def broadcastCommand(host, cmd):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.sendto(cmd, host)
+        return s.recv(1024)
+    except:
+        return None
+    finally:
+        if s != None:
+            s.close()
+
+def sendCommand(host, cmd):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(host)
+        s.send(cmd)
+        return s.recv(1024)
+    except:
+        return None
+    finally:
+        if s != None:
+            s.close()
+
+def checkSwitch(host):
+    cmd = "AT+RECOSCAN"
+    return broadcastCommand(host, cmd)
+
+def enableSwitch(host):
+    current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    cmd = "AT+YZSWITCH=1,on,%s<CR><LF>" % current_time
+    ret = sendCommand(host, cmd)
+    if re.match("^\+ok", ret):
+        return True
+    else:
+        return False
+
+def disableSwitch(host):
+    current_time = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    cmd = "AT+YZSWITCH=1,off,%s<CR><LF>" % current_time
+    ret = sendCommand(host, cmd)
+    if re.match("^\+ok", ret):
+        return True
+    else:
+        return False
+
+def querySwitch(host):
+    cmd = "AT+YZOUT<CR><LF>"
+    return sendCommand(host, cmd)
+
+def main():
+    run()
+
+def run():
+    switch_addr = "192.168.2.31"
+    switch_port = 8899
+
+    with open("/root/records.log", "w+") as records:
+        with open("/root/autocontrol.log", "w+") as log:
+            while True:
+                try:
+                    s = serial.Serial("/dev/ttyS0", 57600)
+                    message = s.readline()
+                    if message.startswith("CMD:"):
+                        log.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - "  + message)
+                        if "TURN_ON_SWITCH" in message:
+                            log.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - Turn on the switch")
+                            enableSwitch((switch_addr, switch_port))
+                        elif "TURN_OFF_SWITCH" in message:
+                            log.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - Turn off the switch")
+                            disableSwitch((switch_addr, switch_port))
+                        log.flush()
+                    elif message.startswith("REC:"):
+                        records.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - "  + message)
+                        records.flush()
+                    else:
+                        log.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - "  + message)
+                        log.flush()
+                except Exception as e:
+                    log.write(time.strftime("%Y-%m-%d %H:%M:%S") + " - " + e)
+                    log.flush()
+                    continue
+
+if __name__=="__main__":
+    main()
